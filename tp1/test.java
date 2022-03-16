7f530c40da79be7cd101b0025b99f3764a4dccc3
mneus <maxence.neus@polytech-lille.fr>
2022-03-16 10:20:29 +0100

    TP1 Q8

--- tp1/test.java
+++ tp1/test.java
@@ -2,16 +2,38 @@ package tp1;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.Box.Filler;
 
 public class test {
     public static void main(String args[]) {
         JFrame win = new JFrame();
-        win.getContentPane().setLayout(new GridLayout(4, 4));
+        win.getContentPane().setLayout(new BoxLayout(win.getContentPane(), BoxLayout.Y_AXIS));
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        for (int i = 0; i<16; i++) {
-            win.getContentPane().add(new JButton("Button" + i));
-        }
+        win.getContentPane().add(Box.createHorizontalGlue());
+
+        JButton button1 = new JButton("Button 1");
+        JButton button2 = new JButton("Button 2");
+        JButton button3 = new JButton("Button 3");
+        Filler filler = new Filler(null, null, null);
+
+        win.getContentPane().add(button1);
+        win.getContentPane().add(button2);
+        win.getContentPane().add(filler);
+        win.getContentPane().add(button3);
+        
+        button1.add(Box.createHorizontalGlue());
+        button1.add(Box.createVerticalGlue());
+        
+        filler.add(Box.createHorizontalGlue());
+        filler.add(Box.createVerticalGlue());
+
+        button2.add(Box.createHorizontalGlue());
+        button2.add(Box.createVerticalGlue());
+        
+        button3.add(Box.createHorizontalGlue());
+        button3.add(Box.createVerticalGlue());
+    
 
         win.setPreferredSize(new Dimension(400, 300));
         win.setVisible(true);        
